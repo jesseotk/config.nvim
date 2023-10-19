49704aeb064406c8aa19da90d5502571c4ff14b3
Jesse <139448699+ogjojo@users.noreply.github.com>
2023-10-19 21:06:38 +0300

    Update init.lua, migrate indent-blankline from version 2 to 3

--- init.lua
+++ init.lua
@@ -114,13 +114,13 @@ require('lazy').setup({
   {
     -- Add indentation guides even on blank lines
     'lukas-reineke/indent-blankline.nvim',
-    -- Enable `lukas-reineke/indent-blankline.nvim`
     -- See `:help indent_blankline.txt`
+    main = "ibl",
     opts = {
-      char = '┊',
-      show_trailing_blankline_indent = false,
+      indent = { char = '┊' },
     },
   },
+
 
   -- "gc" to comment visual regions/lines
   { 'numToStr/Comment.nvim', opts = {} },
